e8780adc687f6ceff66d75bfbc0367a418ab0945
jhonncamarg0 <jhonatha.emmanuel@academico.ifpb.edu.br>
2022-08-18 00:36:03 -0300

    Programação C1

--- Challenges ONE/Good Lock/index.js
+++ Challenges ONE/Good Lock/index.js
@@ -1,36 +1,39 @@
 document.getElementById("texto_saida").style.display = "none";
 
-var botao_criptografia = document.getElementById("texto_criptografia");
-var botao_descriptografia = document.getElementById("texto_descriptografia");
+let botao_criptografia = document.getElementById("texto_criptografia");
+let botao_descriptografia = document.getElementById("texto_descriptografia");
 
 function mostrarCodigo() {
-    var texto = document.getElementById("texto_saida");
-    var imagem = document.getElementById("texto_aviso_saida");
+    let texto = document.getElementById("texto_saida");
+    let imagem = document.getElementById("texto_aviso_saida");
     imagem.style.display = "none";
     texto.style.display = "block";
 }
 
 function criptografia() {
-    var codigo = document.getElementById("texto_entrada").value;
-    var codigo2 = document.getElementById("texto_saida").value;
-    codigo.replace("a", "ai");
-    codigo.replace("e", "enter");
-    codigo.replace("i", "imes");
-    codigo.replace("o", "ober");
-    codigo.replace("u", "ufat");
-    mostrarCodigo()
-    codigo2.value = codigo.value;
+    let codigo = document.getElementById("texto_entrada").value;
+    let e = codigo.replace(/e/g, "enter");
+    let o = e.replace(/o/g, "ober");
+    let i = o.replace(/i/g, "imes");
+    let a = i.replace(/a/g, "ai");
+    let u = a.replace(/u/g, "ufat");
+    console.log(u);
+    mostrarCodigo();
+    let codigo2 = document.getElementById("texto_saida");
+    codigo2.innerText = u;
 }
 
 function descriptografia() {
-    var codigo = document.getElementById("texto_entrada").value;
-    var codigo2 = document.getElementById("texto_saida").value;
-    codigo.replace("ai", "a");
-    codigo.replace("enter", "e");
-    codigo.replace("imes", "i");
-    codigo.replace("ober", "o");
-    codigo.replace("ufat", "u");
-    codigo2.value = codigo.value;
+    let codigo = document.getElementById("texto_entrada").value;
+    let e = codigo.replace(/enter/g, "e");
+    let o = e.replace(/ober/g, "o");
+    let i = o.replace(/imes/g, "i");
+    let a = i.replace(/ai/g, "a");
+    let u = a.replace(/ufat/g, "u");
+    console.log(u);
+    mostrarCodigo();
+    let codigo2 = document.getElementById("texto_saida");
+    codigo2.innerText = u;
 }
 
 botao_criptografia.onclick = criptografia;
