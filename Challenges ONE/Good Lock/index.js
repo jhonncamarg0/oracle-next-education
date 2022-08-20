89c1b650ca71d574029b9d602e421e1789cfa655
jhonncamarg0 <jhonatha.emmanuel@academico.ifpb.edu.br>
2022-08-20 20:03:25 -0300

    fixes

--- Challenges ONE/Good Lock/index.js
+++ Challenges ONE/Good Lock/index.js
@@ -1,73 +1,115 @@
 document.getElementById("texto_saida").style.display = "none";
 document.getElementById("texto_copiar").style.display = "none";
+/* document.getElementById("texto_controle").style.display = "none"; */
 
 let botao_criptografia = document.getElementById("texto_criptografia");
 let botao_descriptografia = document.getElementById("texto_descriptografia");
 let botao_copiar = document.getElementById("texto_copiar");
+/* let botao_controle = document.getElementById("texto_controle"); */
+/* var controle = true; */
 
 function mostrarCodigo() {
-    let texto = document.getElementById("texto_saida");
-    let botao = document.getElementById("texto_copiar");
     let imagem = document.getElementById("texto_aviso_saida");
+    let texto = document.getElementById("texto_saida");
+    let botao_copy = document.getElementById("texto_copiar");
+    /* let botao_control = document.getElementById("texto_controle"); */
     imagem.style.display = "none";
     texto.style.display = "block";
-    botao.style.display = "block";
+    botao_copy.style.display = "block";
+    /* botao_control.style.display = "inline-block"; */
 }
 
-function criptografia() {
-    let codigo = document.getElementById("texto_entrada").value;
-    let e = codigo.replace(/e/g, "enter");
-    let o = e.replace(/o/g, "ober");
-    let i = o.replace(/i/g, "imes");
-    let a = i.replace(/a/g, "ai");
-    let u = a.replace(/u/g, "ufat");
-    mostrarCodigo();
-    let codigo2 = document.getElementById("texto_saida");
-    codigo2.innerText = u;
-    botao_copiar.innerText = "Copiar texto criptografado";
-    let clear = document.getElementById("texto_entrada");
-    clear.value = "";
-    Swal.fire({
-        title: "Criptografado!",
-        text: "",
-        icon: "info",
-        confirmButtonText: "Fechar"
-      })
-}
-
-function descriptografia() {
-    let codigo = document.getElementById("texto_entrada").value;
-    let e = codigo.replace(/enter/g, "e");
-    let o = e.replace(/ober/g, "o");
-    let i = o.replace(/imes/g, "i");
-    let a = i.replace(/ai/g, "a");
-    let u = a.replace(/ufat/g, "u");
-    mostrarCodigo();
-    let codigo2 = document.getElementById("texto_saida");
-    codigo2.innerText = u;
-    botao_copiar.innerText = "Copiar texto descriptografado";
-    let clear = document.getElementById("texto_entrada");
-    clear.value = "";
-    Swal.fire({
-        title: "Descriptografado!",
-        text: "",
-        icon: "info",
-        confirmButtonText: "Fechar"
-      })
-}
-
-function copiar() {
-    let codigo = document.getElementById("texto_saida");
-    codigo.select();
+function copiarCodigo() {
+    let codigoC = document.getElementById("texto_saida");
+    codigoC.select();
     document.execCommand("copy");
     Swal.fire({
         title: "Copiado!",
         text: "",
         icon: "info",
         confirmButtonText: "Fechar"
-      })
+    });
+}
+
+/*
+function resetarControle() {
+    controle = true;
+}
+*/
+
+function criptografia() {
+    var codigo = document.getElementById("texto_entrada").value;
+    if (codigo != "") {
+        let e = codigo.replace(/e/g, "enter");
+        let o = e.replace(/o/g, "ober");
+        let i = o.replace(/i/g, "imes");
+        let a = i.replace(/a/g, "ai");
+        let u = a.replace(/u/g, "ufat");
+        mostrarCodigo();
+        botao_copiar.innerText = "Copiar texto criptografado";
+        let codigo2 = document.getElementById("texto_saida");
+        codigo2.innerText = u;
+        let clear = document.getElementById("texto_entrada");
+        clear.value = "";
+        Swal.fire({
+            title: "Criptografado!",
+            text: "",
+            icon: "info",
+            confirmButtonText: "Fechar"
+        });
+    }
+    else if (codigo == "") {
+        Swal.fire({
+            title: "Texto vazio!",
+            text: "Insira algum texto para ser criptografado",
+            icon: "info",
+            confirmButtonText: "Fechar"
+        });
+    }
+}
+
+function descriptografia() {
+    let codigo = document.getElementById("texto_entrada").value;
+    if (codigo != "") {
+        if (codigo.includes("enter") || codigo.includes("ober") || codigo.includes("imes") ||
+        codigo.includes("ai") || codigo.includes("ufat") == true) {
+            let e = codigo.replace(/enter/g, "e");
+            let o = e.replace(/ober/g, "o");
+            let i = o.replace(/imes/g, "i");
+            let a = i.replace(/ai/g, "a");
+            let u = a.replace(/ufat/g, "u");
+            mostrarCodigo();
+            botao_copiar.innerText = "Copiar texto descriptografado";
+            let codigo2 = document.getElementById("texto_saida");
+            codigo2.innerText = u;
+            let clear = document.getElementById("texto_entrada");
+            clear.value = "";
+            Swal.fire({
+                title: "Descriptografado!",
+                text: "",
+                icon: "info",
+                confirmButtonText: "Fechar"
+            });
+        }
+        else {
+            Swal.fire({
+                title: "Texto incompatível!",
+                text: "Esse texto não foi criptografado pelo Good Lock",
+                icon: "info",
+                confirmButtonText: "Fechar"
+            });
+        }
+    }
+    else if (codigo == "") {
+        Swal.fire({
+            title: "Texto vazio!",
+            text: "Insira algum texto criptografado pelo Good Lock para ser descriptografado",
+            icon: "info",
+            confirmButtonText: "Fechar"
+        });
+    }
 }
 
 botao_criptografia.onclick = criptografia;
 botao_descriptografia.onclick = descriptografia;
-botao_copiar.onclick = copiar;
+botao_copiar.onclick = copiarCodigo;
