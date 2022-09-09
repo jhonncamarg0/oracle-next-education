95fa52f7ec8fc685bb96518e7cf98754c44439ed
jhonncamarg0 <jhonatha.emmanuel@academico.ifpb.edu.br>
2022-09-09 19:05:03 -0300

    UI

---
+++ Challenges ONE/Medieval Gallow/index.js
@@ -0,0 +1,36 @@
+document.getElementById("inicial_pagina").style.display = "none";
+document.getElementById("palavra_pagina").style.display = "none";
+document.getElementById("forca_pagina").style.display = "block";
+
+var botao_jogo = document.getElementById("jogo_botao");
+var botao_palavra = document.getElementById("palavra_botao");
+
+var botao_salvar = document.getElementById("salvar_botao");
+var botao_cancelar = document.getElementById("cancelar_botao");
+
+var botao_desistir = document.getElementById("desistir_botao");
+
+botao_jogo.onclick = function () {
+    document.getElementById("inicial_pagina").style.display = "none";
+    document.getElementById("forca_pagina").style.display = "block";
+}
+
+botao_palavra.onclick = function () {
+    document.getElementById("inicial_pagina").style.display = "none";
+    document.getElementById("palavra_pagina").style.display = "block";
+}
+
+botao_salvar.onclick = function () {
+    document.getElementById("palavra_pagina").style.display = "none";
+    document.getElementById("forca_pagina").style.display = "block";
+}
+
+botao_cancelar.onclick = function () {
+    document.getElementById("palavra_pagina").style.display = "none";
+    document.getElementById("inicial_pagina").style.display = "block";
+}
+
+botao_desistir.onclick = function () {
+    document.getElementById("forca_pagina").style.display = "none";
+    document.getElementById("inicial_pagina").style.display = "block";
+}
