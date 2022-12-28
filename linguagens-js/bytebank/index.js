381f13cb32c910212b544b1cf1b20216347201f0
jhonncamarg0 <jhonemmaje@gmail.com>
2022-12-28 17:02:17 -0300

    upload

--- linguagens-js/bytebank/index.js
+++
@@ -1,8 +0,0 @@
-import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-
-const cliente1 = new Cliente("Ricardo", 123);
-const cliente2 = new Cliente("Alice", 456);
-
-const conta1 = new ContaCorrente(1001, cliente1);
-const conta2 = new ContaCorrente(1002, cliente2);
